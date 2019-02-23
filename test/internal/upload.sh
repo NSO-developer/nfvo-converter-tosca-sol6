5de69fd8f96204c1e745c0318cb1b7ad4868f803
Aaron Steele <aasteele@cisco.com>
2019-02-22 16:55:25 -0800

    Some personal testing scripts

--- test/internal/upload.sh
+++ test/internal/upload.sh
@@ -1,1 +1,5 @@
 #!/usr/bin/env bash
+scp ../../outputs/output_vCU.json admin@$anu_vm:~/aaron/
+scp ../../outputs/output_vDU.json admin@$anu_vm:~/aaron/
+scp ../../outputs/output_esc.json admin@$anu_vm:~/aaron/
+scp ../../outputs/output_VPEC_SI.json admin@$anu_vm:~/aaron/
