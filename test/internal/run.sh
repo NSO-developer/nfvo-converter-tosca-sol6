436bbb8b7af42f35dd42f75e172bf81679911187
Aaron Steele <aasteele@cisco.com>
2019-02-22 16:55:25 -0800

    Some personal testing scripts

--- test/internal/run.sh
+++ test/internal/run.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
-export PYTHONPATH=python/tailf_etsi_rel2_nfvo_tosca
+export PYTHONPATH=../../python/tailf_etsi_rel2_nfvo_tosca
 
 # Run the four examples that the program shouldn't *at least* crash with
-vpc_vnfd=examples/standalone_vpc_vnfd_esc_4_4.yaml
-vcu=examples/altiostar_vCU.yaml
-vdu=examples/altiostar_vDU.yaml
-vpec=examples/VPEC_SI_UPP_vnfd_esc_4_4.yaml
+tosca=../../tosca.py
+vpc_vnfd=../../examples/standalone_vpc_vnfd_esc_4_4.yaml
+vcu=../../examples/altiostar_vCU.yaml
+vdu=../../examples/altiostar_vDU.yaml
+vpec=../../examples/VPEC_SI_UPP_vnfd_esc_4_4.yaml
 echo Run $vpc_vnfd...
-/usr/local/bin/python3 tosca.py -f $vpc_vnfd -o outputs/output_esc.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vpc_vnfd -o ../../outputs/output_esc.json --dry-run -H -p
 echo Run $vdu...
-/usr/local/bin/python3 tosca.py -f $vcu -o outputs/output_vCU.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vcu -o ../../outputs/output_vCU.json --dry-run -H -p
 echo Run $vdu...
-/usr/local/bin/python3 tosca.py -f $vdu -o outputs/output_vDU.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vdu -o ../../outputs/output_vDU.json --dry-run -H -p
 echo Run $vpec...
-/usr/local/bin/python3 tosca.py -f $vpec -o outputs/output_VPEC_SI.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vpec -o ../../outputs/output_VPEC_SI.json --dry-run -H -p
