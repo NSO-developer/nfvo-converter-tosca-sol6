2249c364874dd176a9aeb53e0365d10fb454830d
Aaron Steele <aasteele@cisco.com>
2019-02-25 14:19:49 -0800

    Fixed a typo

--- freeze.sh
+++ freeze.sh
@@ -4,4 +4,4 @@ echo Creating build file...
 if [ -d "dist" ]; then
   rm -r dist
 fi
-pyinstaller -F -p python/tailf_etsi_rel_2_nfvo_tosca tosca.py
+pyinstaller -F -p python/tailf_etsi_rel2_nfvo_tosca tosca.py
