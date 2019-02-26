2dc42ab6f049ac229574b4e83b61431ce2c104be
Aaron Steele <aasteele@cisco.com>
2019-02-26 13:50:54 -0800

    Moving files around

--- freeze.sh
+++ freeze.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 echo Creating build file...
-if [ -d "dist" ]; then
+if [[ -d "dist" ]]; then
   rm -r dist
 fi
-pyinstaller -F -p python/tailf_etsi_rel2_nfvo_tosca \
---exclude-module variables solcon.py
+pyinstaller -F -p python/tailf_etsi_rel2_nfvo_tosca solcon.py
--- test/internal/run.sh
+++ test/internal/run.sh
@@ -3,15 +3,16 @@ export PYTHONPATH=../../python/tailf_etsi_rel2_nfvo_tosca
 
 # Run the four examples that the program shouldn't *at least* crash with
 tosca=../../solcon.py
+config=../../config.toml
 vpc_vnfd=../../examples/standalone_vpc_vnfd_esc_4_4.yaml
 vcu=../../examples/altiostar_vCU.yaml
 vdu=../../examples/altiostar_vDU.yaml
 vpec=../../examples/VPEC_SI_UPP_vnfd_esc_4_4.yaml
 echo Run $vpc_vnfd...
-/usr/local/bin/python3 $tosca -f $vpc_vnfd -o ../../outputs/output_esc.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vpc_vnfd -o ../../outputs/output_esc.json -c $config -H -p
 echo Run $vdu...
-/usr/local/bin/python3 $tosca -f $vcu -o ../../outputs/output_vCU.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vcu -o ../../outputs/output_vCU.json -c $config -H -p
 echo Run $vdu...
-/usr/local/bin/python3 $tosca -f $vdu -o ../../outputs/output_vDU.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vdu -o ../../outputs/output_vDU.json -c $config -H -p
 echo Run $vpec...
-/usr/local/bin/python3 $tosca -f $vpec -o ../../outputs/output_VPEC_SI.json --dry-run -H -p
+/usr/local/bin/python3 $tosca -f $vpec -o ../../outputs/output_VPEC_SI.json -c $config -H -p
