668b9ea29875b363ae109f6fd3ab2d08080feba7
Aaron Steele <aasteele@cisco.com>
2019-06-14 15:30:20 -0700

    Started writing tests for vcu
    Signed-off-by: Aaron Steele <aasteele@cisco.com>

--- test/units/esc/test_VPC_SI.py
+++ test/units/esc/test_VPC_SI.py
@@ -5,7 +5,8 @@ from src.util import Util
 class TestVPCSI(unittest.TestCase):
 
     def setUp(self):
-        self.vpc_si = Util.solcon("VPEC_SI_UPP_vnfd_esc_4_4.yaml")
+        self.vnfd = None
+       #self.vnfd = Util.solcon("VPEC_SI_UPP_vnfd_esc_4_4.yaml")
 
     def test_one(self):
         self.assertEqual(1, 1)
--- test/units/esc/test_vPE.py
+++ test/units/esc/test_vPE.py
@@ -5,7 +5,8 @@ from src.util import Util
 class TestvPE(unittest.TestCase):
 
     def setUp(self):
-        self.vPE = Util.solcon("Tosca_vPE.yaml")
+        self.vnfd = None
+        #self.vnfd = Util.solcon("Tosca_vPE.yaml")
 
     def test_one(self):
         self.assertEqual(1, 1)
