972986f4349a839f13352c42c18ff486f3505895
Aaron Steele <aasteele@cisco.com>
2019-06-14 14:29:19 -0700

    Fixing creating too many blank ext-cpd array elements, plus started adding test files
    Signed-off-by: Aaron Steele <aasteele@cisco.com>

--- src/util.py
+++ src/util.py
@@ -6,17 +6,16 @@ Test util file, it should be moved into test/ in the future
 
 class Util:
     @staticmethod
-    def solcon(file, config="config-esc.toml"):
+    def solcon(file, config="config-esc.toml", root="../"):
         # Disable
-        sys.stdout = open(os.devnull, 'w')
-        s = SolCon(internal_run=True, internal_args=Util.format_args(file, con=config))
-        sys.stdout = sys.__stdout__
+        s = SolCon(internal_run=True, internal_args=Util.format_args(file, con=config, root=root))
         return s.cnfv
 
     @staticmethod
-    def format_args(f, con, r=None):
-        c = "../../config/{}".format(con)
-        s = "../../config/config-sol6.toml"
-        l_l = "CRITICAL"
-        a = {"f": "../../examples/esc/{}".format(f), "o": None, "c": c, "s": s, "r": r, "l": l_l}
+    def format_args(f, con, r=None, root=""):
+        c = "{}config/{}".format(root, con)
+        s = "{}config/config-sol6.toml".format(root)
+        l_l = 50  # critical
+        o_silent = True
+        a = {"f": "{}examples/esc/{}".format(root, f), "o": None, "c": c, "s": s, "r": r, "l": l_l, "e": o_silent}
         return a
